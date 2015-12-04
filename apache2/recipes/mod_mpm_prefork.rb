17289d33f497026772fbdf737522d07828c1dbf5
lucjan-stepien-tabc <lucjan.stepien@ticket-text.com>
2015-12-04 15:32:46 +0000

    added support for apache version 2.2 - mpm_prefork - mpm_worker

--- apache2/recipes/mod_mpm_prefork.rb
+++ apache2/recipes/mod_mpm_prefork.rb
@@ -1,9 +1,17 @@
 # apache_module "mpm_prefork"
 
-apache_module "mpm_event" do
-	enable false 
+if platform_family?('debian')
+  if node[:apache][:version] == '2.2'
+  	package "apache2-mpm-prefork"
+  else
+    apache_module "mpm_event" do
+      enable false 
+    end
+    apache_module "mpm_worker" do
+      enable false 
+    end
+    apache_module "mpm_prefork" 
+  end
+elsif platform_family?('rhel')
+  log 'Place holder for rhel'
 end
-apache_module "mpm_worker" do
-	enable false 
-end
-apache_module "mpm_prefork" 
--- apache2/recipes/mod_mpm_worker.rb
+++ apache2/recipes/mod_mpm_worker.rb
@@ -1,9 +1,17 @@
 # apache_module "mpm_worker"
 
-apache_module "mpm_event" do
-	enable false 
+if platform_family?('debian')
+  if node[:apache][:version] == '2.2'
+    package "apache2-mpm-worker"
+  else
+    apache_module "mpm_event" do
+      enable false 
+    end
+    apache_module "mpm_prefork" do
+      enable false 
+    end
+    apache_module "mpm_worker" 
+  end
+elsif platform_family?('rhel')
+  log 'Place holder for rhel'
 end
-apache_module "mpm_prefork" do
-	enable false 
-end
-apache_module "mpm_worker" 
